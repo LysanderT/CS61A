ad1c2bc0a49888b7086aac529031fa5c66152607
Lysander <121090088@link.cuhk.edu.cn>
2022-07-12 23:53:28 +0800

    hw07

--- hw/hw07/hw07.scm
+++ hw/hw07/hw07.scm
@@ -1,48 +1,96 @@
 (define (cddr s)
   (cdr (cdr s)))
 
+
 (define (cadr s)
-  'YOUR-CODE-HERE
+  (car (cdr s))
 )
 
 (define (caddr s)
-  'YOUR-CODE-HERE
+  (car (cdr (cdr s)))
 )
 
 
 (define (sign num)
-  'YOUR-CODE-HERE
+  (cond ((> num 0) 1)
+    ((< num 0) -1)
+    (else 0))
 )
 
 
 (define (square x) (* x x))
 
 (define (pow x y)
-  'YOUR-CODE-HERE
+  (if (= y 1)
+    x
+    (if (even? y)
+      (square (pow x (/ y 2)))
+      (* x (square (pow x (/ (- y 1) 2)) ))
+    )
+  )
 )
 
 
+(define (contains list x)
+	(cond [(null? list) #f]
+		[(equal? (car list) x) #t]
+		[else (contains (cdr list) x)]))
+
+
 (define (unique s)
-  'YOUR-CODE-HERE
+; method 1
+  (define (helper s lst)
+    (if (null? s) lst
+      (if (contains lst (car s))
+        (helper (cdr s) lst)
+        (helper (cdr s) (append lst (list (car s))))
+      )
+    )
+  )
+  (helper s ())
+)
+
+(define (unique s)
+; method 2
+  (if (null? s) nil 
+    (cons
+      (car s) 
+      (unique (filter (lambda (x) (not (equal? x (car s)))) (cdr s)))
+    )
+  )
 )
 
 
 (define (replicate x n)
-  'YOUR-CODE-HERE
+  (define (helper x n lst)
+    (if (= n 0)
+      lst
+      (helper x (- n 1) (append lst (list x)))
+    )
   )
+  (helper x n ())
+)
 
 
 (define (accumulate combiner start n term)
-  'YOUR-CODE-HERE
+  (if (= n 0)
+    start
+    (combiner (term n) (accumulate combiner start (- n 1) term))
+  )
 )
 
 
 (define (accumulate-tail combiner start n term)
-  'YOUR-CODE-HERE
+  (define (helper result combiner n term)
+    (if (= n 0)
+      result
+      (helper (combiner (term n) result) combiner (- n 1) term)
+    )  
+  )
+  (helper start combiner n term)
 )
 
 
 (define-macro (list-of map-expr for var in lst if filter-expr)
-  'YOUR-CODE-HERE
+  `(map (lambda (,var) ,map-expr) (filter (lambda (,var) ,filter-expr) ,lst))
 )
-
