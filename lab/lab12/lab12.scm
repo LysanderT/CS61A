aa1b208eca317af0b535eeb70e7ae9c040cde5e3
Lysander <121090088@link.cuhk.edu.cn>
2022-07-13 13:33:13 +0800

    lab12

--- lab/lab12/lab12.scm
+++ lab/lab12/lab12.scm
@@ -1,6 +1,8 @@
 
 (define-macro (def func args body)
-    'YOUR-CODE-HERE)
+    (list `define (cons func args)
+      body)
+)
 
 
 (define (map-stream f s)
@@ -8,18 +10,27 @@
     	nil
     	(cons-stream (f (car s)) (map-stream f (cdr-stream s)))))
 
+
 (define all-three-multiples
-  'YOUR-CODE-HERE
+  (map-stream (lambda (x) (+ x 3)) (cons-stream 0 all-three-multiples))
 )
 
 
 (define (compose-all funcs)
-  'YOUR-CODE-HERE
+  (if (null? funcs)
+    (lambda (x) x)
+    (lambda (x) ((compose-all (cdr funcs)) ((car funcs) x)))
+  )
 )
 
 
 (define (partial-sums stream)
-  'YOUR-CODE-HERE
-  (helper 0 stream)
+    (define (helper result s)
+        (if (null? s)
+            ()
+            (cons-stream (+ result (car s)) (helper (+ result (car s)) (cdr-stream s)))   
+        )
+    )
+    (helper 0 stream)
 )
 
